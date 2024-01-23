c6758009a6c6071e91dce3a0943df6048ad0930f
Zhao-yihang <157504045+Zhao-yihang@users.noreply.github.com>
2024-01-23 23:20:50 +0800

    Update Hamitonian_MAE.py

--- Hamitonian_MAE.py
+++ Hamitonian_MAE.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 # 读取Excel文件
-file_path = 'test.xlsx'  # 请替换为你的文件路径
+file_path = 'Hamitonian_MAE.xlsx'  # 请替换为你的文件路径
 df = pd.read_excel(file_path, index_col=0)
 
 # 使用seaborn库绘制热力图
@@ -14,6 +14,6 @@ plt.xlabel('Alpha')
 plt.ylabel('Beta')
 
 # 保存图像为高分辨率文件（300ppi）
-output_file_path = 'output.png'  # 请替换为你的输出文件路径
+output_file_path = 'Hamitonian_MAE_output.png'  # 请替换为你的输出文件路径
 heatmap.get_figure().savefig(output_file_path, dpi=300)
 plt.show()
